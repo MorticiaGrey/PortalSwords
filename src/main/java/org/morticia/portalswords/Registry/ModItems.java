9c2ef0dcbd8799253ec6fdc9d3f32a0ca905b765
Morticia <sunseed218@gmail.com>
2021-07-15 19:24:54 -0600

    Added hellstalker

--- src/main/java/org/morticia/portalswords/Registry/ModItems.java
+++ src/main/java/org/morticia/portalswords/Registry/ModItems.java
@@ -11,7 +11,7 @@ import org.morticia.portalswords.items.Riftstalker;
 
 public class ModItems {
     // Swords
-    public static Firestalker FIRESTALKER = new Firestalker(new PortalSwordToolMaterial(), 3, -2.0F, new Item.Settings().group(ModGroups.PORTAL_SWORDS_ITEM_GROUP).fireproof().rarity(Rarity.EPIC));
+    public static Firestalker FIRESTALKER = new Firestalker(new PortalSwordToolMaterial(), 5, -2.0F, new Item.Settings().group(ModGroups.PORTAL_SWORDS_ITEM_GROUP).fireproof().rarity(Rarity.EPIC));
     public static Riftstalker RIFTSTALKER = new Riftstalker(new PortalSwordToolMaterial(), 3, -2.0F, new Item.Settings().group(ModGroups.PORTAL_SWORDS_ITEM_GROUP).fireproof().rarity(Rarity.EPIC));
 
     // Crafting Materials
@@ -19,14 +19,22 @@ public class ModItems {
     public static Item REINFORCEDHANDGUARD = new Item(new Item.Settings().group(ModGroups.PORTAL_SWORDS_ITEM_GROUP).fireproof());
     public static Item RIFTBLADE = new Item(new Item.Settings().group(ModGroups.PORTAL_SWORDS_ITEM_GROUP).fireproof());
 
+    public static Item FIERYHANDLE = new Item(new Item.Settings().group(ModGroups.PORTAL_SWORDS_ITEM_GROUP).fireproof());
+    public static Item FIERYHANDGUARD = new Item(new Item.Settings().group(ModGroups.PORTAL_SWORDS_ITEM_GROUP).fireproof());
+    public static Item HELLBLADE = new Item(new Item.Settings().group(ModGroups.PORTAL_SWORDS_ITEM_GROUP).fireproof());
+
     public static void initItems() {
+        // Swords
+        Registry.register(Registry.ITEM, new Identifier(portalswords.MOD_ID, "firestalker"), FIRESTALKER);
+        Registry.register(Registry.ITEM, new Identifier(portalswords.MOD_ID, "riftstalker"), RIFTSTALKER);
+
         // Crafting Materials
         Registry.register(Registry.ITEM, new Identifier(portalswords.MOD_ID, "reinforced_handle"), REINFORCEDHANDLE);
         Registry.register(Registry.ITEM, new Identifier(portalswords.MOD_ID, "reinforced_hand_guard"), REINFORCEDHANDGUARD);
         Registry.register(Registry.ITEM, new Identifier(portalswords.MOD_ID, "rift_blade"), RIFTBLADE);
 
-        // Swords
-        //Registry.register(Registry.ITEM, new Identifier(portalswords.MOD_ID, "firestalker"), FIRESTALKER);
-        Registry.register(Registry.ITEM, new Identifier(portalswords.MOD_ID, "riftstalker"), RIFTSTALKER);
+        Registry.register(Registry.ITEM, new Identifier(portalswords.MOD_ID, "fiery_handle"), FIERYHANDLE);
+        Registry.register(Registry.ITEM, new Identifier(portalswords.MOD_ID, "fiery_handguard"), FIERYHANDGUARD);
+        Registry.register(Registry.ITEM, new Identifier(portalswords.MOD_ID, "hell_blade"), HELLBLADE);
     }
 }
--- src/main/java/org/morticia/portalswords/items/Firestalker.java
+++ src/main/java/org/morticia/portalswords/items/Firestalker.java
@@ -1,19 +1,31 @@
 package org.morticia.portalswords.items;
 
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.TeleportTarget;
 import net.minecraft.world.World;
 
 public class Firestalker extends SwordItem {
     public Firestalker(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
         super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        target.setOnFireFor(7);
+        return super.postHit(stack, target, attacker);
     }
 
     // Keep in mind, code is run on server AND client
@@ -23,25 +35,29 @@ public class Firestalker extends SwordItem {
             return TypedActionResult.consume(this.asItem().getDefaultStack());
         }
         if (world instanceof ServerWorld && !player.hasVehicle() && !player.hasPassengers() && player.canUsePortals()) {
-            /*RegistryKey<World> registryKey = world.getRegistryKey() == World.NETHER ? World.OVERWORLD : World.NETHER;
-            ServerWorld serverWorld = ((ServerWorld)world).getServer().getWorld(registryKey);
-            if (serverWorld == null) {
-                return TypedActionResult.pass(this.asItem().getDefaultStack());
-            }*/
-
             ServerWorld serverWorld = (ServerWorld)world;
             MinecraftServer minecraftServer = serverWorld.getServer();
             RegistryKey<World> registryKey = world.getRegistryKey() == World.NETHER ? World.OVERWORLD : World.NETHER;
             ServerWorld serverWorld2 = minecraftServer.getWorld(registryKey);
 
-            if (serverWorld2 != null && minecraftServer.isNetherAllowed() && !player.hasVehicle()) {
-                world.getProfiler().push("portal");
-                player.resetNetherPortalCooldown();
-                player.moveToWorld(serverWorld2);
-                world.getProfiler().pop();
+            if (serverWorld2 == null) {
+                return TypedActionResult.pass(this.asItem().getDefaultStack());
             }
 
-            //player.moveToWorld(serverWorld);
+            serverWorld2.setChunkForced(0, 0, true);
+            int y = serverWorld2.getChunkManager().getWorldChunk(0, 0).getHighestNonEmptySection().getYOffset();
+
+            for (int x = -2; x <= 3; x++) {
+                for (int z = -2; z <= 3; z++) {
+                    serverWorld2.setBlockState(new BlockPos(x, y - 2, z), Blocks.NETHER_BRICKS.getDefaultState());
+                }
+            }
+
+            TeleportTarget target = new TeleportTarget(new Vec3d(0, y, 0), player.getVelocity(), player.prevYaw, player.prevPitch);
+            if (player instanceof ServerPlayerEntity) {
+                ((ServerPlayerEntity) player).teleport(serverWorld2, target.position.x, target.position.y, target.position.z, target.yaw, target.pitch);
+            }
+
             return TypedActionResult.consume(this.asItem().getDefaultStack());
         }
         return TypedActionResult.pass(this.asItem().getDefaultStack());
